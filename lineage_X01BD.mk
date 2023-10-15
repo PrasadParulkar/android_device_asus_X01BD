f81279b54f1c3b6304e8135b85d62b9b18a7bdab
RohitTiwari <rohitttiwari52@gmail.com>
2023-10-15 19:43:39 +0530

    X01BD: Import makefiles for RisingOS

---
+++ lineage_X01BD.mk
@@ -0,0 +1,44 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit some common RisingOS stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1440
+
+# Build Flags
+WITH_GMS := true
+RISING_MAINTAINER := RohitTiwari
+RISING_CHIPSET := SDM660
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USE_GOOGLE_TELEPHONY := false
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Inherit from X01BD device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := lineage_X01BD
+PRODUCT_DEVICE := X01BD
+PRODUCT_BRAND := Asus
+PRODUCT_MODEL := Zenfone Max Pro M2
+PRODUCT_MANUFACTURER := Asus
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE=ASUS_X01BD_1 \
+    PRODUCT_NAME=WW_X01BD \
+    PRIVATE_BUILD_DESC="sdm660_64-user 10 QKQ1 73 release-keys"
+
+# Build fingerprint
+BUILD_FINGERPRINT := "asus/WW_X01BD/ASUS_X01BD_1:10/QKQ1/17.2018.2012.434-20201203:user/release-keys"

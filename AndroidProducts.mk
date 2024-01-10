b9bfc64b8b822f1e11d328b14ba8f679d5104d21
Rohit Tiwari <rohitttiwari52@gmail.com>
2024-01-10 11:06:37 +0530

    X01BD: Import PixelOS makefiles & add all lunch combo
    Signed-off-by: Rohit Tiwari <rohitttiwari52@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_X01BD.mk
+    $(LOCAL_DIR)/evolution_X01BD.mk \
+    $(LOCAL_DIR)/aosp_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
     evolution_X01BD-userdebug \
     evolution_X01BD-user \
-    evolution_X01BD-eng
+    evolution_X01BD-eng \
+    aosp_X01BD-userdebug \
+    aosp_X01BD-user \
+    aosp_X01BD-eng

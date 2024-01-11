f0eeb6228cc620ed46e8d5ccaad2283420691493
Rohit Tiwari <rohitttiwari52@gmail.com>
2024-01-12 01:19:40 +0530

    X01BD: Import lineageos makefiles
    Signed-off-by: Rohit Tiwari <rohitttiwari52@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,7 +6,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/evolution_X01BD.mk \
-    $(LOCAL_DIR)/aosp_X01BD.mk
+    $(LOCAL_DIR)/aosp_X01BD.mk \
+    $(LOCAL_DIR)/lineage_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
     evolution_X01BD-userdebug \
@@ -14,4 +15,7 @@ COMMON_LUNCH_CHOICES := \
     evolution_X01BD-eng \
     aosp_X01BD-userdebug \
     aosp_X01BD-user \
-    aosp_X01BD-eng
+    aosp_X01BD-eng \
+    lineage_X01BD-userdebug \
+    lineage_X01BD-user \
+    lineage_X01BD-eng

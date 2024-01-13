a55bb9b0060f8d7dd30af59030151aef93f595c0
Rohit Tiwari <rohitttiwari52@gmail.com>
2024-01-13 12:28:10 +0530

    X01BD: Bringup Project-Elixir
    Signed-off-by: Rohit Tiwari <rohitttiwari52@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,7 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X01BD.mk
+    $(LOCAL_DIR)/aosp_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X01BD-userdebug
+    aosp_X01BD-userdebug \
+    aosp_X01BD-user \
+    aosp_X01BD-eng
--- aosp_X01BD.mk
+++ aosp_X01BD.mk
@@ -9,14 +9,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Project-Elixir stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from X01BD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# Elixir Officials
+ELIXIR_BUILD_TYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_INCLUDE_STOCK_ACORE := false
+
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X01BD
+PRODUCT_NAME := aosp_X01BD
 PRODUCT_DEVICE := X01BD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M2

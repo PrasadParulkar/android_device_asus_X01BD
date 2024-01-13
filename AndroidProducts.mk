709c14efd9bbf699b91bc9aa3d939988309b9942
Rohit Tiwari <rohitttiwari52@gmail.com>
2024-01-13 09:56:22 +0530

    X01BD: Import makefiles for `AOSP` & `EvolutionX`
    Signed-off-by: Rohit Tiwari <rohitttiwari52@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,7 +5,17 @@
 #
 
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/evolution_X01BD.mk \
+    $(LOCAL_DIR)/aosp_X01BD.mk \
     $(LOCAL_DIR)/lineage_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X01BD-userdebug
+    evolution_X01BD-userdebug \
+    evolution_X01BD-user \
+    evolution_X01BD-eng \
+    aosp_X01BD-userdebug \
+    aosp_X01BD-user \
+    aosp_X01BD-eng \
+    lineage_X01BD-userdebug \
+    lineage_X01BD-user \
+    lineage_X01BD-eng
--- aosp_X01BD.mk
+++ aosp_X01BD.mk
@@ -9,19 +9,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-# Inherit from X01BD device
-$(call inherit-product, $(LOCAL_PATH)/device.mk)
-
-# Build Flags
+# PixelExperience Properties
 TARGET_BOOT_ANIMATION_RES := 1440
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
 
+# Inherit from X01BD device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X01BD
+PRODUCT_NAME := aosp_X01BD
 PRODUCT_DEVICE := X01BD
 PRODUCT_BRAND := Asus
 PRODUCT_MODEL := Zenfone Max Pro M2
@@ -30,6 +30,9 @@ PRODUCT_MANUFACTURER := Asus
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE=ASUS_X01BD \
-    PRODUCT_NAME=WW_X01BD
+    PRODUCT_DEVICE=ASUS_X01BD_1 \
+    PRODUCT_NAME=WW_X01BD \
+    PRIVATE_BUILD_DESC="sdm660_64-user 10 QKQ1 73 release-keys"
 
+# Build fingerprint
+BUILD_FINGERPRINT := "asus/WW_X01BD/ASUS_X01BD_1:10/QKQ1/17.2018.2012.434-20201203:user/release-keys"

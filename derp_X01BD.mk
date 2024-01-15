51f1c53afae2dad7a45cd382ed90335216fe3c84
Rohit Tiwari <rohitttiwari52@gmail.com>
2024-01-15 09:39:05 +0530

    X01BD: Welcome DerpFest
    Signed-off-by: Rohit Tiwari <rohitttiwari52@gmail.com>

--- derp_X01BD.mk
+++ derp_X01BD.mk
@@ -9,23 +9,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common Project-Elixir stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common DerpFest stuff
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit from X01BD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Elixir Officials
-ELIXIR_BUILD_TYPE := OFFICIAL
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
-TARGET_INCLUDE_STOCK_ACORE := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_X01BD
+PRODUCT_NAME := derp_X01BD
 PRODUCT_DEVICE := X01BD
-PRODUCT_BRAND := asus
+PRODUCT_BRAND := Asus
 PRODUCT_MODEL := Zenfone Max Pro M2
 PRODUCT_MANUFACTURER := asus
 
